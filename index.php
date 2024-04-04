78d2496772dfa39a58ea30653f8812e881f7dc78
kkommus <keiti.k6mmus@gmail.com>
2024-04-04 20:21:13 +0300

    gg

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php
+//***objektorjenteeritud***//
 //sinu andmed
 $db_server = 'localhost';
 $db_andmebaas = 'drooniralli';
 $db_kasutaja = 'keiti';
 $db_salasona = 'sh2mp00n';
-
-//ühendus andmebaasiga
-$yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
-//ühenduse kontroll
+//yhenduse loomine
+$yhendus = new mysqli($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
+// ühenduse kontroll
 if(!$yhendus){
 	die('Ei saa ühendust andmebaasiga');
 }
-
 //päring
-$paring = 'SELECT * FROM drooniralli';
-$valjund = mysqli_query($yhendus, $paring);
-
-
-
+$paring = 'SELECT * FROM drooniralli';	
+$valjund = $yhendus->query($paring);
+//väljund
+while($rida = $valjund->fetch_row()){
+	var_dump($rida);
+	}
+//ühenduste sulgemine
+$yhendus->close();
 ?>
